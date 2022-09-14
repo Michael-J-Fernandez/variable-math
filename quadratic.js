0473c101218de0dbb00d9837a5f5cdb6bfc1a2fb
Michael-J-Fernandez <michael.fernandez@codeimmersives.com>
2022-09-14 10:35:05 -0400

    Complete distance and quadratic assignments. Clean up rest of code.

--- quadratic.js
+++ quadratic.js
@@ -1,4 +1,9 @@
-let a = 
-let b = 
-let c = 
+let a = 1
+let b = 4
+let c = -21
 
+let x1 = (-b + Math.sqrt((b**2)-(4*a*c))) / (2*a);
+console.log(x1);
+
+let x2 = (-b - Math.sqrt((b**2)-(4*a*c))) / (2*a);
+console.log(x2);
